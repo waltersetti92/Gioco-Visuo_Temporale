db3bda53a6864a05af676e865857476d98406d00
Walter Setti <walter.setti@iit.it>
2021-01-23 16:16:51 +0100

    First Scenario

--- Visuo_Tempo_WS/Visuo_Tempo_WS/Interaction.Designer.cs
+++ Visuo_Tempo_WS/Visuo_Tempo_WS/Interaction.Designer.cs
@@ -28,20 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Font = new System.Drawing.Font("Segoe Script", 150F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.Color.Lime;
+            this.timerLabel.Location = new System.Drawing.Point(608, 248);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(279, 317);
+            this.timerLabel.TabIndex = 0;
+            this.timerLabel.Text = "5";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Interaction
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.timerLabel);
             this.Name = "Interaction";
             this.Size = new System.Drawing.Size(1047, 490);
             this.Load += new System.EventHandler(this.Interaction_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Visuo_Tempo_WS/Visuo_Tempo_WS/Interaction.cs
+++ Visuo_Tempo_WS/Visuo_Tempo_WS/Interaction.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,14 +14,17 @@ namespace Visuo_Tempo_WS
     public partial class Interaction : UserControl
     {
         public Main parentForm { get; set; }
+        public int timeleft = 6;
         public Interaction()
         {
             InitializeComponent();
+           
         }
 
         private void Interaction_Load(object sender, EventArgs e)
         {
-
+            timer1.Enabled = true;
+            parentForm.playbackResourceAudio("MC");
         }
         public void setPos(int w, int h)
         {
@@ -30,6 +34,21 @@ namespace Visuo_Tempo_WS
             Width = w - 1 * offset;
             Height = h - 1 * offset;
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (timeleft > 0)
+            {
+                timeleft= timeleft-1;
+                timerLabel.Text = timeleft.ToString();
+                parentForm.playbackResourceAudio("MC");
+            }
+            else if (timeleft == 0)
+            {
+                timer1.Stop();
+            }
         }
     }
 }
